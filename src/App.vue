09422c8c48b1d12797919046804bca345d700986
Takahashi Tomohiko <takahashi@tomohiko.io>
2020-04-04 18:26:03 +0900

    リスト

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <button @click="createNewTodo">Add Todo</button>
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{todo.name}} - {{todo.description}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,6 +15,7 @@
   import API, {  graphqlOperation } from '@aws-amplify/api';
   // eslint-disable-next-line
   import { createTodo } from "./graphql/mutations";
+  import { listTodos } from './graphql/queries'
 
   /*
   Supported log levels for browser debugging:
@@ -21,13 +28,26 @@
   */
 
   window.LOG_LEVEL = 'VERBOSE';
+
   export default {
     name: 'app',
+    data(){
+      return {
+        todos: []
+      }
+    },
     methods :{
       async createNewTodo(){
         const todo = { name: "Todo Title" , description: "あれをやる" + Date()}
         await API.graphql(graphqlOperation(createTodo, { input: todo }))
+      },
+      async getData(){
+        const todoData = await API.graphql(graphqlOperation(listTodos))
+        this.todos.push(...this.todos, ...todoData.data.listTodos.items);
       }
+    },
+    created(){
+      this.getData()
     }
   };
 </script>
